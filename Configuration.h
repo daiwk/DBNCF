558934f668de9436b367ed184c9d950c9c22470b
daiwk <daiwk1989@gmail.com>
2014-03-26 12:35:42 +0800

    runsh..conf

--- Configuration.h
+++ Configuration.h
@@ -122,10 +122,10 @@ namespace Config {
         static const int CD_STEPS = 1;
         static const double EPS_W = 0.0015;
         static const double EPS_VB = 0.0012;
-        static const double EPS_HB = 0.1;
+        static const double EPS_HB = 0.001;
         static const double EPS_D = 0.001;
         static const double WEIGHT_COST = 0.0001;
-        static const double MOMENTUM = 0.9;
+        static const double MOMENTUM = 0.95;
         static const bool ANNEALING = true;
         static const double ANNEALING_RATE = 2.;
 
@@ -182,11 +182,11 @@ namespace Config {
     }
 
 	namespace AHRBMCF {
-        static const int TRAIN_EPOCHS = 1; // AHRBMCF的训练轮数
+        static const int TRAIN_EPOCHS = 30; // AHRBMCF的训练轮数
         static const int BATCH_SIZE = 100;  // 默认100;
 	// HL是HiddenLayer的缩写
-        static const int HL_SIZE = 2;  // 默认20;
-        static const int HL_NUM = 2;  // 默认2;
+        static const int HL_SIZE = 20;  // 默认20;
+        static const int HL_NUM = 3;  // 默认2;
         // Misc
         static const bool VERBOSE = true;
     }
--- run_ahrbmcf_mpi.sh
+++ run_ahrbmcf_mpi.sh
@@ -20,6 +20,13 @@ function run_small_mpi()
 
 	type=$1
 	F=$2
+	
+	epoch=30
+	batch=100
+	hsize=20
+	annealing=2
+	hlnum=3
+	eps_b=0.01 #default 0.1
 
 	LS=${datadir}/"data/bin/LS.bin.small-changeUserNum-1"
 	TS=${datadir}/"data/bin/TS.bin.small-changeUserNum-1"
@@ -38,9 +45,9 @@ function run_small_mpi()
 		--QS ${TS} 
 		--VS ${TS} 
 		--F ${F} 
-		--openmp ${type} > ${logdir}/${type}_${log}_${F}_epoch_10_hsize_20
+		--openmp ${type} > ${logdir}/${type}_${log}_${F}_${epoch}_${batch}_${hsize}_${hnum}_${annealing}_${eps_b}
 		--verbose 1 
-		--log ${rmsedir}/${type}_${rmse}_${F}_epoch_10_hsize_20  "
+		--log ${rmsedir}/${type}_${rmse}_${F}_${epoch}_${batch}_${hsize}_${hnum}_${annealing}_${eps_b}"
 
 	echo ${run_cmd}
 	eval ${run_cmd}
@@ -57,6 +64,13 @@ function run_full_mpi()
 
 	type=$1
 	F=$2
+
+	epoch=30
+	batch=100
+	hsize=20
+	annealing=2
+	hlnum=3
+	eps_b=0.01 #default 0.1
 
 	LS=${datadir}/"data/bin/LS.bin.fengge-changeUserNum-1"
 	TS=${datadir}/"data/bin/TS.bin.fengge-changeUserNum-1"
@@ -75,9 +89,9 @@ function run_full_mpi()
 		--QS ${TS} 
 		--VS ${TS} 
 		--F ${F} 
-		--openmp ${type} > ${logdir}/${type}_${log}_${F}_epoch_10_hsize_20
+		--openmp ${type} > ${logdir}/${type}_${log}_${F}_${epoch}_${batch}_${hsize}_${hnum}_${annealing}_${eps_b}
 		--verbose 1 
-		--log ${rmsedir}/${type}_${rmse}_${F}_epoch_10_hsize_20  "
+		--log ${rmsedir}/${type}_${rmse}_${F}_${epoch}_${batch}_${hsize}_${hnum}_${annealing}_${eps_b}"
 
 	echo ${run_cmd}
 	eval ${run_cmd}
